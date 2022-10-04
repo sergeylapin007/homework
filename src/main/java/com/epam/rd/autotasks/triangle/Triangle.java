8aa048e4f293e6ac3db6e3d9643755ea89785962
Serhii Lapin <Serhii_Lapin@epam.com>
2022-10-04 23:44:26 +0200

    Solved

--- src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
+++ src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
@@ -2,16 +2,39 @@ package com.epam.rd.autotasks.triangle;
 
 class Triangle {
 
+    Point first;
+    Point second;
+    Point third;
+
+    double xA;
+    double yA;
+    double xB;
+    double yB;
+    double xC;
+    double yC;
+
     public Triangle(Point a, Point b, Point c) {
-        //TODO
+         this.first = a;
+         this.second = b;
+         this.third = c;
+         this.xA = first.getX();
+         this.yA = first.getY();
+         this.xB = second.getX();
+         this.yB = second.getY();
+         this.xC = third.getX();
+         this.yC = third.getY();
     }
 
     public double area() {
-        //TODO
+        double square = 0.5 * (xA * (yB - yC) - xB * (yA - yC) + xC *(yA - yB));
+        return Math.abs(square);
     }
 
     public Point centroid(){
-        //TODO
+        double temp1 = (xA + xB + xC) / 3;
+        double temp2 = (yA + yB + yC) / 3;
+        Point centroid = new Point(temp1,temp2);
+        return centroid;
     }
 
 }
